a62d7094e44fde663fcf4f563b50c44f02578c4d
harsh <harshrpatel93@gmail.com>
2019-07-12 17:50:25 -0400

    fully functioning

--- db/schema.sql
+++ db/schema.sql
@@ -6,12 +6,12 @@ USE notes_db;
 CREATE TABLE notes(
   id INT(10) NOT NULL AUTO_INCREMENT,
   title VARCHAR(50) NOT NULL,
-  body TEXT(300) NOT NULL,
+  textarea TEXT(300) NOT NULL,
   primary key(id)
 );
 
 INSERT INTO notes
- (title, body)
+ (title, textarea)
 VALUES ("Note-taker", "Must finish the note-taker homework before saturday night"),
  ("Update portfolio", "been a while since updated the updated portfolio"),
  ("Update portfolio", "been a while since updated the updated portfolio"),
